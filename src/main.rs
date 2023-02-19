e8ea059bf7ea229b1f4906af8207e2ecdad94145
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2023-02-19 13:56:41 +1000

    Refactored to PlayerBundle

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,31 @@ const SCREEN_HEIGHT: f32 = 400.0;
 const SCREEN_GRID_WIDTH: f32 = 80.0;
 const SCREEN_GRID_HEIGHT: f32 = 50.0;
 
+const SPRITE_ANIMATION_COUNT: usize = 3;
+const SPRITE_ANIMATION_DELAY: f32 = 0.1;
+const SPRITE_DEFAULT_X: f32 = 39.0;
+const SPRITE_DEFAULT_Y: f32 = 19.0;
+const SPRITE_DIRECTION_DELAY: f32 = 1.5;
+const SPRITE_HEIGHT: f32 = 72.0;
+const SPRITE_WIDTH: f32 = 72.0;
+
+const TITLE: &str = "Bevy Roguelike";
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Bundle)]
+struct PlayerBundle {
+    animation_timer: AnimationTimer,
+    direction: Direction,
+    direction_timer: DirectionAnimationTimer,
+    position: Position,
+    _p: Player,
+
+    #[bundle]
+    sprite: SpriteSheetBundle,
+}
+
 #[derive(Component)]
 struct Position {
     x: f32,
@@ -23,12 +48,12 @@ enum Direction {
 struct AnimationTimer(Timer);
 
 #[derive(Component, Deref, DerefMut)]
-struct FacingAnimationTimer(Timer);
+struct DirectionAnimationTimer(Timer);
 
 fn animate_direction(
     time: Res<Time>,
     mut query: Query<(
-        &mut FacingAnimationTimer,
+        &mut DirectionAnimationTimer,
         &mut Direction
     )>
 ) {
@@ -59,7 +84,7 @@ fn animate_sprite(
     for (mut timer, mut sprite, mut transform, mut position, facing) in &mut query {
         timer.tick(time.delta());
         if timer.just_finished() {
-            sprite.index = (sprite.index + 1) % 3 + (3 * *facing as usize);
+            sprite.index = (sprite.index + 1) % SPRITE_ANIMATION_COUNT + (SPRITE_ANIMATION_COUNT * *facing as usize);
             match facing {
                 Direction::Down => {
                     position.as_mut().y -= SCREEN_HEIGHT / SCREEN_GRID_HEIGHT;
@@ -85,20 +110,23 @@ fn setup(
     mut texture_atlases: ResMut<Assets<TextureAtlas>>
 ) {
     let texture_handle = asset_server.load("character.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(72.0, 72.0), 3, 4, None, None);
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(SPRITE_WIDTH, SPRITE_HEIGHT), 3, 4, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            transform: Transform::from_scale(Vec3::splat(1.0)),
-            ..default()
-        },
-        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-        Direction::Down,
-        FacingAnimationTimer(Timer::from_seconds(1.0, TimerMode::Repeating)),
-        Position{ x: 39.0, y: 24.0 }
-    ));
+    commands.spawn(
+        PlayerBundle {
+            animation_timer: AnimationTimer(Timer::from_seconds(SPRITE_ANIMATION_DELAY, TimerMode::Repeating)),
+            direction: Direction::Down,
+            direction_timer: DirectionAnimationTimer(Timer::from_seconds(SPRITE_DIRECTION_DELAY, TimerMode::Repeating)),
+            position: Position { x: SPRITE_DEFAULT_X, y: SPRITE_DEFAULT_Y },
+            _p: Player,
+            sprite: SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                transform: Transform::from_xyz(SPRITE_DEFAULT_X, SPRITE_DEFAULT_Y, 0.0),
+                ..default()
+            },
+        }
+    );
 }
 
 fn main() {
@@ -107,9 +135,9 @@ fn main() {
             .set(ImagePlugin::default_nearest())
             .set(WindowPlugin {
                 window: WindowDescriptor {
-                    title: "Bevy Roguelike".to_string(),
-                    width: 768.,
-                    height: 480.,
+                    title: TITLE.to_string(),
+                    width: SCREEN_WIDTH,
+                    height: SCREEN_HEIGHT,
                     present_mode: PresentMode::AutoVsync,
                     ..default()
                 },
